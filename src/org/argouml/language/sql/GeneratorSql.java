7b3d826abd594999f55179aca48a9a224e7264c3
Kai Drahmann <drahmann@tigris.org>
2007-01-15 15:37:38 +0000

    - created interface that should be implemented by code-generators - added some code-creation functionality in GeneratorSql - created a class that only contains important attributes for a column definition
    git-svn-id: http://argouml-sql.tigris.org/svn/argouml-sql/trunk@15 dc9a2ffb-671e-0410-bcaf-86adf31cdc5b

--- src/org/argouml/language/sql/GeneratorSql.java
+++ src/org/argouml/language/sql/GeneratorSql.java
@@ -24,84 +24,147 @@
 
 package org.argouml.language.sql;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
+import org.argouml.model.Model;
 import org.argouml.uml.generator.CodeGenerator;
-
+import org.argouml.uml.generator.SourceUnit;
 
 /**
  * SQL generator
  */
 class GeneratorSql implements CodeGenerator {
+    private static final String LINE_SEPARATOR = System
+            .getProperty("line.separator");
+
     /**
      * The instances.
      */
     private static final GeneratorSql INSTANCE = new GeneratorSql();
-    
+
+    private Collection foreignKeys;
+
+    private Collection primaryKeys;
+
+    private Collection indices;
+
     /**
      * Constructor.
      */
     private GeneratorSql() {
-	// Cannot be created from somewhere else.
+        // Cannot be created from somewhere else.
     }
 
     /**
      * @return the singleton instance.
      */
     public static GeneratorSql getInstance() {
-	return INSTANCE;
+        return INSTANCE;
     }
 
     /**
      * Generate code for the specified classifiers. If generation of
-     * dependencies is requested, then every file the specified elements
-     * depends on is generated too (e.g. if the class MyClass has an attribute
-     * of type OtherClass, then files for OtherClass are generated too).
-     *
-     * @param elements the UML model elements to generate code for.
-     * @param deps Recursively generate dependency files too.
+     * dependencies is requested, then every file the specified elements depends
+     * on is generated too (e.g. if the class MyClass has an attribute of type
+     * OtherClass, then files for OtherClass are generated too).
+     * 
+     * @param elements
+     *            the UML model elements to generate code for.
+     * @param deps
+     *            Recursively generate dependency files too.
      * @return A collection of {@link org.argouml.uml.generator.SourceUnit}
      *         objects. The collection may be empty if no file is generated.
-     * @see org.argouml.uml.generator.CodeGenerator#generate(
-     *         Collection, boolean)
+     * @see org.argouml.uml.generator.CodeGenerator#generate( Collection,
+     *      boolean)
      */
     public Collection generate(Collection elements, boolean deps) {
-	
-	throw new Error("Not yet implemented");
+        // Just some testing for understanding the model and facade
+        StringBuffer sb = new StringBuffer();
+        SqlCodeCreator creator = new FirebirdSqlCodeCreator();
+
+        Iterator it = elements.iterator();
+        while (it.hasNext()) {
+            Object element = it.next();
+
+            if (Model.getFacade().isAClass(element)) {
+                List colDefs = getColumnDefinitions(element);
+                String s = creator.createTable(Model.getFacade().getName(element),
+                        colDefs);
+                sb.append(s);
+            }
+        }
+
+        String sourceCode = sb.toString();
+        SourceUnit su = new SourceUnit("E:\\Test.sql", sourceCode);
+        Collection result = new ArrayList();
+        result.add(su);
+        return result;
+    }
+
+    private List getColumnDefinitions(Object element) {
+        List columnDefinitions = new ArrayList();
+        Iterator itAttributes = Model.getFacade().getAttributes(element)
+                .iterator();
+        while (itAttributes.hasNext()) {
+            Object attribute = itAttributes.next();
+
+            ColumnDefinition cd = new ColumnDefinition();
+            cd.setName(Model.getFacade().getName(attribute));
+
+            Object type = Model.getFacade().getType(attribute);
+            cd.setDatatype(Model.getFacade().getName(type));
+            
+            // Collection stereotypes =
+            // Model.getFacade().getStereotypes(attribute);
+            // cd.setNullable(???)
+
+            columnDefinitions.add(cd);
+        }
+        
+        return columnDefinitions;
     }
 
     /**
      * Generate files for the specified classifiers.
+     * 
      * @see #generate(Collection, boolean)
-     * @param elements the UML model elements to generate code for.
-     * @param path The source base path.
-     * @param deps Recursively generate dependency files too.
+     * @param elements
+     *            the UML model elements to generate code for.
+     * @param path
+     *            The source base path.
+     * @param deps
+     *            Recursively generate dependency files too.
      * @return The filenames (with relative path) as a collection of Strings.
-     * The collection may be empty if no file will be generated.
-     * @see org.argouml.uml.generator.CodeGenerator#generateFiles(
-     *         Collection, String, boolean)
+     *         The collection may be empty if no file will be generated.
+     * @see org.argouml.uml.generator.CodeGenerator#generateFiles( Collection,
+     *      String, boolean)
      */
-    public Collection generateFiles(
-	    Collection elements,
-	    String path,
-	    boolean deps) {
-	Collection units = generate(elements, deps);
-	throw new Error("Not yet implemented");
+    public Collection generateFiles(Collection elements, String path,
+            boolean deps) {
+        return generate(elements, deps);
     }
 
     /**
      * Returns a list of files that will be generated from the specified
      * modelelements.
+     * 
      * @see #generate(Collection, boolean)
-     * @param elements the UML model elements to generate code for.
-     * @param deps Recursively generate dependency files too.
+     * @param elements
+     *            the UML model elements to generate code for.
+     * @param deps
+     *            Recursively generate dependency files too.
      * @return The filenames (with relative path) as a collection of Strings.
-     * The collection may be empty if no file will be generated.
+     *         The collection may be empty if no file will be generated.
      * @see org.argouml.uml.generator.CodeGenerator#generateFileList(
-     *         Collection, boolean)
+     *      Collection, boolean)
      */
     public Collection generateFileList(Collection elements, boolean deps) {
-	throw new Error("Not yet implemented");
+        throw new Error("Not yet implemented");
     }
 
 } /* end class GeneratorSql */
---
+++ src/org/argouml/language/sql/SqlCodeCreator.java
@@ -0,0 +1,80 @@
+// $Id: eclipse-argo-codetemplates.xml 11347 2006-10-26 22:37:44Z linus $
+// Copyright (c) 2007 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.language.sql;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface SqlCodeCreator {
+    /**
+     * Method to generate a CREATE TABLE statement. The code for all columns
+     * is also generated. 
+     * 
+     * @param tableName The name of the table.
+     * @param columnDefinitions List of ColumnDefinitions for the table.
+     * @return A string containing the SQL-Code.
+     */
+    String createTable(String tableName, List columnDefinitions);
+
+    /**
+     * Creates sql for a foreign key. The sql for foreign keys is generated
+     * after the sql for all tables is generated.
+     * 
+     * @param tableName
+     *            The name of the table containing the foreign key.
+     * @param columnNames
+     *            A list of strings containing the names of the columns the
+     *            foreign key consists of.
+     * @param referencesTableName
+     *            The name of the table the foreign key references to.
+     * @param referencesColumnNames
+     *            A list of strings containing the names of the referenced
+     *            columns.
+     * @param foreignKeyName
+     *            The name of the foreign key.
+     * @return The created sql statement.
+     */
+    String createForeignKey(String tableName, List columnNames,
+            String referencesTableName, List referencesColumnNames,
+            String foreignKeyName);
+
+    /**
+     * Creates sql for an index definition. The sql for index definitions is
+     * generated after the sql for all tables is generated.
+     * 
+     * @param indexName
+     *            The name of the index.
+     * @param tableName
+     *            The table of the index.
+     * @param columnNames
+     *            A list of strings containing the names of the columns the
+     *            index consists of.
+     * @param descending
+     *            Whether the index is descending.
+     * @return The created sql statement.
+     */
+    String createIndex(String indexName, String tableName, List columnNames,
+            boolean descending);
+}
