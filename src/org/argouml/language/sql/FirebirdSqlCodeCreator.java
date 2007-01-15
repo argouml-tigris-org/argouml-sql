633f0fb375564f2b801e90a2fd6d538a23216f67
Kai Drahmann <drahmann@tigris.org>
2007-01-15 16:00:48 +0000

    - added sample sql creator for firebird
    git-svn-id: http://argouml-sql.tigris.org/svn/argouml-sql/trunk@16 dc9a2ffb-671e-0410-bcaf-86adf31cdc5b

---
+++ src/org/argouml/language/sql/FirebirdSqlCodeCreator.java
@@ -0,0 +1,83 @@
+// $Id: eclipse-argo-codetemplates.xml 11347 2006-10-26 22:37:44Z linus $
+// Copyright (c) 2007 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.language.sql;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class FirebirdSqlCodeCreator implements SqlCodeCreator {
+    private static final String LINE_SEPARATOR = System
+            .getProperty("line.separator");
+
+    public String createForeignKey(String tableName, List columnNames,
+            String referencesTableName, List referencesColumnNames,
+            String foreignKeyName) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("ALTER TABLE ");
+        sb.append(tableName);
+        sb.append(" ADD CONSTRAINT ");
+        sb.append(foreignKeyName);
+        sb.append(" FOREIGN KEY (");
+        Iterator it = columnNames.iterator();
+        boolean firstIteration = true;
+        while (it.hasNext()) {
+            if (!firstIteration) {
+                sb.append(",");
+            }
+            String columnName = (String) it.next();
+            sb.append(columnName);
+        }
+        sb.append(") REFERENCES ");
+        sb.append(referencesTableName);
+        sb.append(" (");
+        it = referencesColumnNames.iterator();
+        firstIteration = true;
+        while (it.hasNext()) {
+            if (!firstIteration) {
+                sb.append(",");
+            }
+            String columnName = (String) it.next();
+            sb.append(columnName);
+        }
+        sb.append(");");
+
+        return sb.toString();
+    }
+
+    public String createIndex(String indexName, String tableName,
+            List columnNames, boolean descending) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    public String createTable(String tableName, List columnDefinitions) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("CREATE TABLE ");
+        sb.append(tableName);
+        sb.append(LINE_SEPARATOR);
+        sb.append("();");
+        return sb.toString();
+    }
+}
