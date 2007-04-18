7c8c32523e05df9e14e627eac25823c795bfdb88
Kai Drahmann <drahmann@tigris.org>
2007-04-18 23:21:33 +0000

    - started implementing code-creation-test
    git-svn-id: http://argouml-sql.tigris.org/svn/argouml-sql/trunk@40 dc9a2ffb-671e-0410-bcaf-86adf31cdc5b

--- tests/org/argouml/language/sql/TestCodeCreation.java
+++ tests/org/argouml/language/sql/TestCodeCreation.java
@@ -24,10 +24,78 @@
 
 package org.argouml.language.sql;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import org.argouml.kernel.Project;
+import org.argouml.model.Model;
+import org.argouml.persistence.AbstractFilePersister;
+import org.argouml.persistence.OpenException;
+import org.argouml.persistence.PersistenceManager;
+import org.argouml.ui.ArgoDiagram;
+import org.argouml.uml.diagram.static_structure.ui.UMLClassDiagram;
+import org.argouml.uml.generator.GeneratorHelper;
+import org.argouml.uml.generator.GeneratorManager;
+import org.argouml.uml.generator.Language;
+import org.argouml.uml.generator.SourceUnit;
+
 /**
  * Class for testing code creation.
  * 
  * @author Anne und Kai
  */
 public class TestCodeCreation extends TestCaseSql {
+    public void testCodeCreation() throws OpenException, InterruptedException {
+        String filename = "test.zargo";
+
+        URL url = getClass().getResource(filename);
+        assertTrue("Unintended failure: resource to be tested is not found: "
+                + filename + ", converted to URL: " + url, url != null);
+        AbstractFilePersister persister = PersistenceManager.getInstance()
+                .getPersisterFromFileName(filename);
+        File testfile = new File(url.getFile());
+
+        Project p = persister.doLoad(testfile);
+        ArgoDiagram activeDiagram = (ArgoDiagram) p.getDiagrams().get(0);
+
+        assertTrue(activeDiagram instanceof UMLClassDiagram);
+
+        UMLClassDiagram d = (UMLClassDiagram) activeDiagram;
+        Vector classes = new Vector();
+        List nodes = d.getNodes();
+        Iterator elems = nodes.iterator();
+        while (elems.hasNext()) {
+            Object owner = elems.next();
+            if (!Model.getFacade().isAClass(owner)
+                    && !Model.getFacade().isAInterface(owner)) {
+
+                continue;
+
+            }
+            String name = Model.getFacade().getName(owner);
+            if (name == null || name.length() == 0
+                    || Character.isDigit(name.charAt(0))) {
+
+                continue;
+
+            }
+            classes.addElement(owner);
+        }
+
+        Language lang = GeneratorHelper.makeLanguage(SqlInit.LANGUAGE_NAME);
+        GeneratorManager.getInstance().addGenerator(lang,
+                GeneratorSql.getInstance());
+        Collection files = GeneratorHelper.generate(lang, classes, false);
+
+        assertEquals(1, files.size());
+
+        SourceUnit generatedFile = (SourceUnit) files.iterator().next();
+        String content = generatedFile.getContent();
+
+        // TODO compare generated content with some reference
+    }
 }
